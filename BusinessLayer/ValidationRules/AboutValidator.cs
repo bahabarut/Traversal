08c0284d478021aed2d6f8ba60d97d61ae7857fc
Baha Barut <20201129503@ogr.akdeniz.edu.tr>
2023-10-29 00:13:54 +0300

    Add project files.

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Bu Alan Boş Geçilemez!");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Bu Alan Boş Geçilemez!");
+            RuleFor(x => x.Description).MinimumLength(10).WithMessage("Bu Alan 10 Karakterden Fazla Olmalıdır!");
+        }
+    }
+}
