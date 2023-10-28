1227593020c92b846878c64c73419ab90736fbce
Baha Barut <20201129503@ogr.akdeniz.edu.tr>
2023-10-29 00:58:41 +0300

    Completed layout & partial process

---
+++ TraversalCore/ViewComponents/Home/SliderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCore.ViewComponents.Home
+{
+    public class SliderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
