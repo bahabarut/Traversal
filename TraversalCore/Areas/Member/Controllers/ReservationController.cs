8af9e77f9315b67586dfafbb08219201b53d7608
Baha Barut <20201129503@ogr.akdeniz.edu.tr>
2023-11-04 02:19:31 +0300

    startig to reservation process

--- TraversalCore/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCore/Areas/Member/Controllers/ReservationController.cs
@@ -1,30 +1,57 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TraversalCore.Areas.Member.Controllers
 {
     [Area("Member")]
+    [Route("Member/[controller]/[action]")]
     public class ReservationController : Controller
     {
-
+        DestinationManager dm = new DestinationManager(new EfDestinaitonDal());
+        ReservationManager rm = new ReservationManager(new EfReservationDal());
         public IActionResult MyCurrentReservation()
         {
-            return View();
+            TempData["breadcrumbTitle"] = "Aktif Rezervasyonlarım";
+            var values = rm.TGetListByFilter(x => x.AppUserID == 1 && x.Status=="İşlem Yapıldı");
+            return View(values);
         }
 
         public IActionResult MyOldReservation()
         {
-            return View();
+            TempData["breadcrumbTitle"] = "Geçmiş Rezervasyon";
+            var values = rm.TGetListByFilter(x => x.AppUserID == 1 && x.Status=="İşlem Yapıldı");
+            return View(values);
         }
         [HttpGet]
         public IActionResult NewReservation()
         {
+            TempData["breadcrumbTitle"] = "Yeni Rezervasyon";
+            ViewBag.destinations = GetDestinations();
             return View();
         }
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            p.AppUserID = 1;
+            p.Status = "Onay Bekliyor";
+            rm.TAdd(p);
+            return RedirectToAction("MyCurrentReservation", "Reservation", new { area = "Member" });
+        }
+
+        List<SelectListItem> GetDestinations()
+        {
+            List<SelectListItem> values = (from x in dm.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = $"{x.City} - {x.DayNight} - {x.Price} - {x.Capacity}",
+                                               Value = $"{x.City} - {x.DayNight}"
+                                           }).ToList();
+            return values;
         }
     }
 }
