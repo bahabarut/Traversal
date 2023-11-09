06fbdd1a18f8b89750212d91f907cfcffa70dcbb
Baha Barut <20201129503@ogr.akdeniz.edu.tr>
2023-11-09 18:35:15 +0300

    complete mail process Mailkit

---
+++ TraversalCore/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,48 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TraversalCore.Areas.Admin.Models;
+using TraversalCore.Models;
+
+namespace TraversalCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            TempData["adminBreadcrumb"] = "Mail Gönderme Sayfası";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailRequest p)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            //gondericinin bilgileri
+            MailboxAddress mailBoxAddressFrom = new MailboxAddress("Admin", "bahabarutps@gmail.com");
+            mimeMessage.From.Add(mailBoxAddressFrom);
+
+            //alıcinin bilgileri
+            MailboxAddress mailBoxAddressTo = new MailboxAddress("User", p.ReceiverMail);
+            mimeMessage.To.Add(mailBoxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = p.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = p.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            //client.Authenticate("bahabarutps@gmail.com", "ba5Ha5A9"); 2 parametre alıyor göndericinin maili ve passwordu
+
+            //bu şekilde kullanırken hata verdiğinde 2 faktörlü doğrulamayı açıp bir de şifreli anahtar alcaz bu anahtarı 2.parametreye yazacaz
+            client.Authenticate("bahabarutps@gmail.com", "ba5ha5a9");//2 parametre alıyor göndericinin maili ve passwordu
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
